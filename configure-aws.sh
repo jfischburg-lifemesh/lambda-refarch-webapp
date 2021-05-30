bdb55ba48e79ec78500e9d622904f98435a539bf
Joshua Fischburg <jfischburg@lifemesh.ai>
2021-05-30 18:32:55 -0400

    Ready to roll.

--- configure-aws.sh
+++ configure-aws.sh
@@ -1,3 +1,4 @@
-export AWS_DEFAULT_REGION=<us-east-1>
-export STACK_NAME=<deploy-serverless-reference-architecture>
-./deploy.sh
+export AWS_DEFAULT_REGION=us-east-1
+export STACK_NAME=deploy-serverless-reference-architecture
+export DEPLOYMENT_BUCKET=deploy-lambda-refarch-webapp
+#./deploy.sh
